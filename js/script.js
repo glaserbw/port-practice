98a0b978c5a4d05cca973acbbe434c04b47ed07f
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-10-29 11:59:25 -0700

    updating styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("What's up from the backend?");
+$(document).ready(function() {
+    console.log("What's up from the backend?");
+});
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Close navbar on click
+
+$("a").bind('click', function() {
+    $("input").click()
+    $(this).find("checkbox").click();
+    console.log("button clicked")
+    return true;
+});
+
+
+
+
+// Style Nav on Scroll 
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+ 
